c4d4dd0c94bd7ade5a3fa662e852a28602aadba9
Shannon (Jitera) <developers@jitera.com>
2024-01-03 09:38:38 +0000

    feat: Confirm email logic

--- app/services/users/confirm_email_service.rb
+++ app/services/users/confirm_email_service.rb
@@ -1,19 +1,24 @@
+
 class ConfirmEmailService
   def initialize(token)
     @token = token
   end
 
   def call
-    email_confirmation = EmailConfirmation.find_by(token: @token, confirmed: false)
+    email_confirmation = EmailConfirmation.find_by(token: @token)
 
-    if email_confirmation.nil? || email_confirmation.expires_at < Time.current
+    if email_confirmation.nil?
+      return { error: 'Token is invalid' }
+    elsif email_confirmation.expires_at < Time.current
       return { error: 'Token is invalid or expired' }
     end
 
+    user = User.find_by(id: email_confirmation.user_id)
+    return { error: 'User not found' } if user.nil?
+
     EmailConfirmation.transaction do
-      email_confirmation.update!(confirmed: true)
-      user = User.find(email_confirmation.user_id)
-      user.update!(email_verified: true)
+      email_confirmation.update!(confirmed: true, updated_at: Time.current)
+      user.update!(email_verified: true, updated_at: Time.current)
     end
 
     { success: 'Email has been successfully confirmed.' }
