4cc4300dbe50dc6520f1fb71c3a5b5acef59c26d
Angelina (Jitera) <developers@jitera.com>
2024-01-03 08:28:46 +0000

    feat: User registration logic

--- app/services/users/registration_service.rb
+++ app/services/users/registration_service.rb
@@ -1,5 +1,7 @@
+
 # FILE PATH: /app/services/users/registration_service.rb
 require 'bcrypt'
+require '/app/models/email_confirmation.rb'
 require '/app/models/user.rb'
 require '/app/mailers/user_mailer.rb' # Ensure the UserMailer is required
 
@@ -10,7 +12,7 @@ module Users
     attr_accessor :email, :password, :password_confirmation
 
     validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP }
-    validates :password, presence: true, length: { minimum: 8 }, format: { with: /\A(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}\z/, message: "Password must be at least 8 characters long and include alphanumeric characters." }
+    validates :password, presence: true, length: { minimum: 10 }, format: { with: /\A(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{10,}\z/, message: "Password must be at least 10 characters long and include alphanumeric characters." }
     validates :password_confirmation, presence: true
 
     validate :password_match
@@ -28,9 +30,10 @@ module Users
       password_salt = BCrypt::Engine.generate_salt
       password_hash = BCrypt::Engine.hash_secret(password, password_salt)
       user = User.new(email: email, password_hash: password_hash, password_salt: password_salt, email_verified: false)
+      email_confirmation = EmailConfirmation.new
 
       if user.save
-        token = user.generate_confirmation_token
+        token = generate_confirmation_token(user)
         send_confirmation_email(user, token)
         { success: true, user_id: user.id, email: user.email, email_verified: user.email_verified }
       else
@@ -42,8 +45,14 @@ module Users
 
     private
 
+    def generate_confirmation_token(user)
+      raw, enc = Devise.token_generator.generate(EmailConfirmation, :token)
+      user.email_confirmations.create(token: enc, created_at: Time.now.utc, expires_at: 15.days.from_now, confirmed: false)
+      raw
+    end
+
     def password_match
-      errors.add(:password_confirmation, I18n.t('activerecord.errors.messages.password_mismatch')) if password != password_confirmation
+      errors.add(:password_confirmation, I18n.t('activerecord.errors.messages.confirmation')) if password != password_confirmation
     end
 
     def email_unique?
